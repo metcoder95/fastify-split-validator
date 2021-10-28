954389ca06270b615dabc0b6b86aea8c6e16944d
Carlos Fuentes <me@metcoder.dev>
2021-10-28 17:52:59 +0200

    test: improve testing

--- test/index.test.js
+++ test/index.test.js
@@ -31,22 +31,22 @@ test('Should allow custom AJV instance for querystring', async t => {
       },
       config: {
         schemaBuilders: {
+          // TODO: normalize to query
           querystring: customAjv
         }
       }
     },
-    (req, reply) => { }
+    (req, reply) => {}
   )
 
   try {
-    const res = await server.inject(
-      {
-        method: 'GET',
-        url: '/',
-        query: {
-          msg: ['hello world']
-        }
-      })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/',
+      query: {
+        msg: ['hello world']
+      }
+    })
 
     t.equal(
       res.statusCode,
@@ -87,18 +87,17 @@ test('Should allow custom AJV instance for body', async t => {
         }
       }
     },
-    (req, reply) => { }
+    (req, reply) => {}
   )
 
   try {
-    const res = await server.inject(
-      {
-        method: 'POST',
-        url: '/',
-        payload: {
-          msg: 'hello world'
-        }
-      })
+    const res = await server.inject({
+      method: 'POST',
+      url: '/',
+      payload: {
+        msg: 'hello world'
+      }
+    })
 
     const body = res.json()
 
@@ -136,15 +135,14 @@ test('Should allow custom AJV instance for params', async t => {
         }
       }
     },
-    (req, reply) => { }
+    (req, reply) => {}
   )
 
   try {
-    const res = await server.inject(
-      {
-        method: 'GET',
-        url: '/1'
-      })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/1'
+    })
 
     const body = res.json()
 
@@ -154,6 +152,412 @@ test('Should allow custom AJV instance for params', async t => {
       400,
       'Should coerce the single element array into string'
     )
+  } catch (err) {
+    t.error(err)
+  }
+})
+
+test('Should allow custom AJV instance for headers', async t => {
+  t.plan(2)
+  const customAjv = new AJV({ coerceTypes: false })
+  const server = Fastify()
+
+  server.register(plugin, {})
+
+  server.get(
+    '/',
+    {
+      schema: {
+        headers: {
+          'x-type': {
+            type: 'integer'
+          }
+        }
+      },
+      config: {
+        schemaBuilders: {
+          headers: customAjv
+        }
+      }
+    },
+    (req, reply) => {}
+  )
+
+  try {
+    const res = await server.inject({
+      method: 'GET',
+      path: '/',
+      headers: {
+        'x-type': '1'
+      }
+    })
+
+    const body = res.json()
+
+    // TODO: set into documentation that it's possible the
+    // error formatter doesn't work as expected.
+    // Custom one should be provided
+    t.equal(body.message, 'headers must be integer')
+    t.equal(
+      res.statusCode,
+      400,
+      'Should coerce the single element array into string'
+    )
+  } catch (err) {
+    t.error(err)
+  }
+})
+
+test('Should work with referenced schemas (querystring)', async t => {
+  t.plan(2)
+  const customAjv = new AJV({ coerceTypes: false })
+  const server = Fastify()
+
+  server.addSchema({
+    $id: 'some',
+    type: 'array',
+    items: {
+      type: 'string'
+    }
+  })
+
+  server.register(plugin, {})
+
+  // The issue is at the `Fastify#setSchemaControler` level,
+  // as when adding a new SchemaController the parent is passed
+  // instead of the same old Schema Controller, causing
+  // to lose the reference to the prior registered Schemas.
+  // Reported at: https://github.com/fastify/fastify/issues/3121
+  server.get(
+    '/',
+    {
+      schema: {
+        query: {
+          msg: {
+            $ref: 'some#'
+          }
+        }
+      },
+      config: {
+        schemaBuilders: {
+          querystring: customAjv
+        }
+      }
+    },
+    (req, reply) => {
+      reply.send({ noop: 'noop' })
+    }
+  )
+
+  try {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/',
+      query: {
+        msg: ['hello world']
+      }
+    })
+
+    const body = res.json()
+
+    t.equal(body.message, 'querystring must be array')
+    t.equal(
+      res.statusCode,
+      400,
+      'Should parse the single element array into string'
+    )
+  } catch (err) {
+    t.error(err)
+  }
+})
+
+test('Should work with referenced schemas (params)', { only: true }, async t => {
+  t.plan(2)
+  const customAjv = new AJV({ coerceTypes: false })
+  const server = Fastify()
+
+  server.addSchema({
+    $id: 'some',
+    type: 'integer'
+  })
+
+  server.register(plugin, {})
+
+  server.get(
+    '/:id',
+    {
+      schema: {
+        params: {
+          id: {
+            $ref: 'some#'
+          }
+        }
+      },
+      config: {
+        schemaBuilders: {
+          params: customAjv
+        }
+      }
+    },
+    (req, reply) => {
+      reply.send({ noop: 'noop' })
+    }
+  )
+
+  try {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/1'
+    })
+
+    const body = res.json()
+
+    t.equal(body.message, 'params must be integer')
+    t.equal(res.statusCode, 400, 'Should not coearce the string into integer')
+  } catch (err) {
+    t.error(err)
+  }
+})
+
+test('Should work with referenced schemas (headers)', async t => {
+  t.plan(2)
+  const customAjv = new AJV({ coerceTypes: false })
+  const server = Fastify()
+
+  server.addSchema({
+    $id: 'some',
+    type: 'integer'
+  })
+
+  server.register(plugin, {})
+
+  server.get(
+    '/',
+    {
+      schema: {
+        headers: {
+          'x-id': {
+            $ref: 'some#'
+          }
+        }
+      },
+      config: {
+        schemaBuilders: {
+          headers: customAjv
+        }
+      }
+    },
+    (req, reply) => {
+      reply.send({ noop: 'noop' })
+    }
+  )
+
+  try {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/',
+      headers: {
+        'x-id': '1'
+      }
+    })
+
+    const body = res.json()
+
+    t.equal(body.message, 'headers must be integer')
+    t.equal(res.statusCode, 400, 'Should not coearce the string into integer')
+  } catch (err) {
+    t.error(err)
+  }
+})
+test('Should work with referenced schemas (body)', async t => {
+  t.plan(2)
+  const customAjv = new AJV({ coerceTypes: false })
+  const server = Fastify()
+
+  server.addSchema({
+    $id: 'some',
+    type: 'string'
+  })
+
+  server.register(plugin, {})
+
+  server.post(
+    '/',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          properties: {
+            msg: {
+              $ref: 'some#'
+            }
+          }
+        }
+      },
+      config: {
+        schemaBuilders: {
+          body: customAjv
+        }
+      }
+    },
+    (req, reply) => {
+      reply.send({ noop: 'noop' })
+    }
+  )
+
+  try {
+    const res = await server.inject({
+      method: 'POST',
+      url: '/',
+      payload: {
+        msg: 1
+      }
+    })
+
+    const body = res.json()
+
+    t.equal(body.message, 'body must be string')
+    t.equal(res.statusCode, 400, 'Should not coearce the string into integer')
+  } catch (err) {
+    t.error(err)
+  }
+})
+
+test('Should work with parent schemas', async t => {
+  t.plan(2)
+  const customAjv = new AJV({ coerceTypes: false })
+  const server = Fastify()
+
+  server.addSchema({
+    $id: 'some',
+    type: 'string'
+  })
+
+  server.register((instance, opts, done) => {
+    instance.register(plugin, {})
+
+    instance.post(
+      '/',
+      {
+        schema: {
+          body: {
+            type: 'object',
+            properties: {
+              msg: {
+                $ref: 'some#'
+              }
+            }
+          }
+        },
+        config: {
+          schemaBuilders: {
+            body: customAjv
+          }
+        }
+      },
+      (req, reply) => {
+        reply.send({ noop: 'noop' })
+      }
+    )
+
+    done()
+  })
+
+  try {
+    const res = await server.inject({
+      method: 'POST',
+      url: '/',
+      payload: {
+        msg: 1
+      }
+    })
+
+    const body = res.json()
+
+    t.equal(body.message, 'body must be string')
+    t.equal(res.statusCode, 400, 'Should not coearce the string into integer')
+  } catch (err) {
+    t.error(err)
+  }
+})
+
+test('Should work with parent nested schemas', async t => {
+  t.plan(4)
+  const customAjv = new AJV({ coerceTypes: false })
+  const server = Fastify()
+
+  server.addSchema({
+    $id: 'some',
+    type: 'array',
+    items: {
+      type: 'string'
+    }
+  })
+
+  server.register((instance, opts, done) => {
+    instance.addSchema({
+      $id: 'another',
+      type: 'integer'
+    })
+
+    instance.register((subInstance, opts, innerDone) => {
+      subInstance.register(plugin, {})
+
+      subInstance.post(
+        '/',
+        {
+          schema: {
+            querystring: {
+              msg: {
+                $ref: 'some#'
+              }
+            },
+            headers: {
+              'x-another': {
+                $ref: 'another#' // I cannot find #another schema
+              }
+            }
+          },
+          config: {
+            schemaBuilders: {
+              querystring: customAjv,
+              headers: customAjv
+            }
+          }
+        },
+        (req, reply) => {
+          reply.send({ noop: 'noop' })
+        }
+      )
+
+      innerDone()
+    })
+
+    done()
+  })
+
+  try {
+    const [res1, res2] = await Promise.all([
+      server.inject({
+        method: 'POST',
+        url: '/',
+        query: {
+          msg: ['string']
+        }
+      }),
+      server.inject({
+        method: 'POST',
+        url: '/',
+        headers: {
+          'x-another': '1'
+        }
+      })
+    ])
+
+    t.equal(res1.json().message, 'querystring must be array')
+    t.equal(res1.statusCode, 400, 'Should not coearce the string into array')
+    t.equal(res2.json().message, 'headers must be integer')
+    t.equal(res2.statusCode, 400, 'Should not coearce the string into integer')
   } catch (err) {
     t.error(err)
   }
